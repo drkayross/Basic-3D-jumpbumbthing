050d2b1d5fa71363aa29ddbe5257463f62fb91cf
drkayross <56225926+drkayross@users.noreply.github.com>
2019-10-06 20:12:35 +0100

    Initial Unity Proj commit
    Initial commit of basic assets. Cube, camera and motion only.

---
+++ Game/GameProj/Assets/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+    public float HeightOffset;
+    public float RearOffset;
+
+    public float SpringConstant;
+
+    private Vector3 _currentDisplacement;
+    private Vector3 _targetPosition;
+    private Rigidbody _body;
+    private Rigidbody _myRigidBody;
+
+    private Renderer _renderer;
+    void Start()
+    {
+        _myRigidBody = GetComponent<Rigidbody>();
+        _body = target.GetComponent<Rigidbody>();
+        _currentDisplacement = _body.transform.position - transform.position;
+        _targetPosition = _body.transform.position + new Vector3(0.0f,HeightOffset,0.0f);
+        _renderer = target.GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        bool isMoving = _body.velocity.sqrMagnitude > 0.01f;
+        _renderer.material.SetColor("_Color", isMoving ? Color.green : Color.red);
+        transform.LookAt(_body.transform);
+
+        Vector3 readOffsetDir = isMoving ? new Vector3(-_body.velocity.x, 0.0f, -_body.velocity.z).normalized : new Vector3(-transform.forward.x, 0.0f, -transform.forward.z).normalized;
+        _targetPosition = _body.transform.position + new Vector3(0.0f,HeightOffset,0.0f) + RearOffset*( readOffsetDir ); 
+        _currentDisplacement = _targetPosition - transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, _targetPosition, SpringConstant * Time.fixedDeltaTime);
+    }
+}
---
+++ Game/GameProj/Assets/CubeMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    public float MaxSpeed = 5f;
+    public float JumpHeight = 3f;
+    public float GroundDistance = 0.2f;
+    public float ForceMult = 5f;
+    public LayerMask Ground;
+
+    public GameObject CameraObject;
+    private Rigidbody _body;
+    private Rigidbody _camerabody;
+    private Vector3 _inputs = Vector3.zero;
+    private bool _isGrounded = true;
+    private Transform _groundChecker;
+    // Start is called before the first frame update
+    void Start()
+    {
+         _body = GetComponent<Rigidbody>();
+         _camerabody = CameraObject.GetComponent<Rigidbody>();
+        _groundChecker = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Collider[] hitColliders = Physics.OverlapSphere(_groundChecker.position, GroundDistance, Ground);
+        _isGrounded =  hitColliders.Length > 0;   
+    
+       
+        Vector3 forwardDirection = new Vector3(_camerabody.transform.forward.x, 0.0f, _camerabody.transform.forward.z).normalized;
+        Vector3 rightDirection = new Vector3(_camerabody.transform.right.x, 0.0f, _camerabody.transform.right.z).normalized;
+        if (_body.velocity.magnitude < MaxSpeed)
+        {
+            
+            _body.AddForce( ForceMult * (Input.GetAxis("Vertical") * forwardDirection + Input.GetAxis("Horizontal") * rightDirection));
+        }
+
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+       
+
+    }
+
+}
+
